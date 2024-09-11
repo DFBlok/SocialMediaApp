997a26cc864c1a734397382e6a838a6ac7b14709
DFBlok <s219392862@mandela.ac.za>
2024-09-11 13:31:27 +0200

    modified the layout

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,8 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 //make any route public e.g About page
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
-
+/* const isPublicRoute = createRouteMatcher(["/settings(.*)", "/"]);
+ */
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
     auth().protect();
@@ -13,6 +14,7 @@ export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/",
     // Always run for API routes
     "/(api|trpc)(.*)",
   ],
