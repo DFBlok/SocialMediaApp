6ae2e9c0907784c7e2f3f422f106219ab8b6aa00
DFBlok <s219392862@mandela.ac.za>
2024-09-11 14:34:43 +0200

    added the stories, feed and post section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
 import { Button } from "@/components/ui/button";
+import Stories from "@/components/Stories";
+import Feed from "@/components/Feed";
+import AddPost from "@/components/AddPost";
 
 export default function Home() {
   return (
-    <div className="p-6 font-[family-name:var(--font-geist-sans)]">
-      <h1>Home Page</h1>
-      <Button variant="destructive" className="my-4">
-        Link in Bio
-      </Button>
+    <div className="p-6 font-[family-name:var(--font-geist-sans)] flex gap-6">
+      <div className="hidden xl:block w-[20%]">Left Section</div>
+      <div className="w-full lg:w-[70%] xl:w-[50%]">
+        Middle Section
+        <div className="flex flex-col gap-6">
+          <Stories />
+          <AddPost />
+          <Feed />
+        </div>
+      </div>
+      <div className="hidden lg:block w-[30%]">Right Section</div>
     </div>
   );
 }
---
+++ components/AddPost.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddPost = () => {
+  return (
+    <div>
+      <h1>This is Post</h1>
+    </div>
+  );
+};
+
+export default AddPost;
