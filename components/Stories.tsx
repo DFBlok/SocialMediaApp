b9335d8f123ca3d3b36810cc19b3319f004742a2
DFBlok <s219392862@mandela.ac.za>
2024-09-12 08:41:00 +0200

    Stories page update

--- components/Stories.tsx
+++ components/Stories.tsx
@@ -1,9 +1,82 @@
 import React from "react";
+import Image from "next/image";
 
 const Stories = () => {
   return (
-    <div>
-      <h1>This is Stories</h1>
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm overflow-scroll scrollbar-hide">
+      <div className="flex gap-8 w-max">
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.unsplash.com/photo-1725668104598-376a886f52a5?q=80&w=986&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="stories1"
+            width={80}
+            height={80}
+            className="rounded-full"
+          />
+          <span className="font-medium">monountin girl</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          {/* stories 2 */}
+          <Image
+            src="https://images.unsplash.com/photo-1725668104598-376a886f52a5?q=80&w=986&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="stories1"
+            width={80}
+            height={80}
+            className="rounded-full"
+          />
+          <span className="font-medium">monountin girl</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.unsplash.com/photo-1725668104598-376a886f52a5?q=80&w=986&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="stories1"
+            width={80}
+            height={80}
+            className="rounded-full"
+          />
+          <span className="font-medium">monountin girl</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.unsplash.com/photo-1725668104598-376a886f52a5?q=80&w=986&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="stories1"
+            width={80}
+            height={80}
+            className="rounded-full"
+          />
+          <span className="font-medium">monountin girl</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.unsplash.com/photo-1725668104598-376a886f52a5?q=80&w=986&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="stories1"
+            width={80}
+            height={80}
+            className="rounded-full"
+          />
+          <span className="font-medium">monountin girl</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.unsplash.com/photo-1725668104598-376a886f52a5?q=80&w=986&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="stories1"
+            width={80}
+            height={80}
+            className="rounded-full"
+          />
+          <span className="font-medium">monountin girl</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.unsplash.com/photo-1725668104598-376a886f52a5?q=80&w=986&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="stories1"
+            width={80}
+            height={80}
+            className="rounded-full"
+          />
+          <span className="font-medium">monountin girl</span>
+        </div>
+      </div>
     </div>
   );
 };
