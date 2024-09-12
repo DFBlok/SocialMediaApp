d9a7505548044d94bf8984cb294f96bb1dd0118d
DFBlok <s219392862@mandela.ac.za>
2024-09-12 08:50:04 +0200

    add avatar image

--- components/AddPost.tsx
+++ components/AddPost.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const AddPost = () => {
   return (
-    <div>
-      <h1>This is Post</h1>
+    <div className="p-4 shadow-md bg-white rounded-lg flex gap-4 justify-center text-sm">
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
     </div>
   );
 };
