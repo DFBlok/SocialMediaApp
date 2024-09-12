7ffee38f070e10b6fbe85d26c6fb052c98e0dcea
DFBlok <s219392862@mandela.ac.za>
2024-09-12 11:35:52 +0200

    added the comment section

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Post from "./Post";
 
 const Feed = () => {
   return (
-    <div>
-      <h1>This is feed</h1>
+    <div className="p-4 bg-white shadow-md rounded-lg flex flex-col gap-12">
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
     </div>
   );
 };
