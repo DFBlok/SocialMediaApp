46d9a5b78fd689272c7577c1660fa22474732b71
DFBlok <s219392862@mandela.ac.za>
2024-09-16 12:17:35 +0200

    fix the profile id

---
+++ app/profile/[id]/page.tsx
@@ -0,0 +1,54 @@
+import Feed from "@/components/Feed";
+import RightMenu from "@/components/rightMenu/RightMenu";
+import LeftMenu from "@/components/leftMenu/LeftMenu";
+import Image from "next/image";
+
+export default function ProfilePage() {
+  return (
+    <div className="p-6 font-[family-name:var(--font-geist-sans)] flex gap-6">
+      <div className="hidden xl:block w-[20%]">
+        {/* <LeftMenu type="profile" /> */}
+      </div>
+      <div className="w-full lg:w-[70%] xl:w-[50%]">
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col items-center justify-center">
+            <div className="w-full h-64 relative">
+              <Image
+                src="https://images.unsplash.com/photo-1726096523685-977cb002374c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="nature"
+                fill
+                className="object-cover rounded-md"
+              />
+              <Image
+                src="https://images.unsplash.com/photo-1719937206491-ed673f64be1f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="camera"
+                width={128}
+                height={128}
+                className="w-32 h-32 rounded-full absolute left-0 right-0 mx-auto -bottom-16 ring-4 ring-white"
+              />
+            </div>
+            <h1 className="mt-20 mb-4 text-2xl font-medium">Robert Welker</h1>
+            <div className="flex items-center justify-center gap-12 mb-4">
+              <div className="flex flex-col items-center">
+                <span className="font-medium">100</span>
+                <span className="text-sm">Posts</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">50</span>
+                <span className="text-sm">Followers</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">20</span>
+                <span className="text-sm">Following</span>
+              </div>
+            </div>
+          </div>
+          <Feed />
+        </div>
+      </div>
+      <div className="hidden lg:block w-[30%]">
+        <RightMenu userId="dev" />
+      </div>
+    </div>
+  );
+}
